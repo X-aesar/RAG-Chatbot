846c0af66664934c9ce9a3a31d51158ba108fa6e
X-aesar <eadeyoju@gmail.com>
2026-02-10 08:32:54 +0100

    Fix next.config pdf parsing

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdf-parse"]
-  /* config options here */
+  serverExternalPackages: ["pdf-parse"],
+  webpack: (config) => {
+    config.externals = config.externals || [];
+    config.externals.push({
+      'canvas': 'commonjs canvas'
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
