41e55a31667474f09d1e594bed45d0be179a7f31
X-aesar <eadeyoju@gmail.com>
2026-02-15 18:24:32 +0100

    Fix Link isues

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,10 +17,11 @@ export default function Home() {
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
+          <Link
+        
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             //href="http://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-               href="http://localhost:3000/upload"
+               href="/upload"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -31,15 +33,15 @@ export default function Home() {
               height={16}
             />
             Upload
-          </a>
-          <a
+          </Link>
+          <Link
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="http://localhost:3000/chat"
+            href="/chat"
             target="_blank"
             rel="noopener noreferrer"
           >
             Chatbot
-          </a>
+          </Link>
         </div>
       </main>
     </div>
